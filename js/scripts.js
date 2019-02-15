ebe8ad7c7a4a1f135d8407390aba39e8754bfb87
dorismtae <dorismtae@gmail.com>
2019-02-15 15:14:30 +0300

    written a javasript code for identifying a type of triangle

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var a= prompt("Enter a number :");
+var b= prompt("Enter a number :");
+var c= prompt("Enter a number :");
+var triangleType= ["Equilateral" , "Isosceles", "Scalene"];
+if (a===b && b===c && c===a){
+  alert (triangleType[0]);
+}
+else if (a===b || b===c || c===a){
+  alert (triangleType[1]);
+}
+// else if (a!==b|| b!==c || a!==c)(a+b>c||b+c>a||a+c>b){
+//   alert (triangleType[2]);
+}
