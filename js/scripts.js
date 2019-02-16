9e754b56d45e15e26add7f70a237fd3c5561a053
dorismtae <dorismtae@gmail.com>
2019-02-16 21:03:04 +0300

    homepage styling

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// function
+function track(){
+//USER INTERFACE
+var a= prompt("Enter a number :");
+var b= prompt("Enter a number :");
+var c= prompt("Enter a number :");
+//BUSINESS LOGIC
+var triangleType= ["Equilateral" , "Isosceles", "Scalene"];
+if (a===b && b===c && c===a){
+  alert (triangleType[0]);
+}
+else if (a===b || b===c || c===a){
+  alert (triangleType[1]);
+}
+else if (a+b>c||b+c>a||a+c>b){
+  alert (triangleType[2]);
+}
+}
